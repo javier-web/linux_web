230c89030f406843a9af9e1f43c004cc9b2915d0
javier-web <javier_web@hotmail.com>
2017-06-09 13:50:52 +0200

    1. Corrección de errores 2. Incluye mensaje de Formulario enviado correctamente

--- validacion_formulario/formulario_contacto.php
+++ validacion_formulario/formulario_contacto.php
@@ -3,7 +3,7 @@
 
 <html lang="es">
     <head>
-        <title>Formulario AJAX</title>
+        <title>Validacion Formulario</title>
         <link rel="stylesheet" href="estilos_formulario.css"/>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <meta charset="utf-8"/>
@@ -21,60 +21,77 @@
         <!--VALIDACION DE CAMPOS con saneado-->
 
         <?php
-        /*En un principio no hay errores*/
-        $errores = false;
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
+
+        /* --SANEADO-- */
+
+            function saneado($valor) {
+                $nuevoValor = trim($valor);
+                $nuevoValor = htmlspecialchars($nuevoValor);
+                return $nuevoValor;
+            }
         
+        
+        /* En un principio no hay errores */
+        $errores = false;
+
         $errorNombre = $errorEmail = '';
         if ($_SERVER['REQUEST_METHOD'] == "POST") {
+           
+            
             if (empty($_POST['nombre'])) {
                 $errorNombre = ' El nombre es obligatorio.';
                 $errores = true;
             } else {
                 $nombre = saneado($_POST['nombre']);
             }
+            
+            if (!empty($_POST['telefono'])) {
+                $telefono = saneado($_POST['telefono']);
+            }
+            
             if (empty($_POST['email'])) {
                 $errorEmail = ' El Email es obligatorio.';
                 $errores = true;
             } else {
                 $email = saneado($_POST['email']);
                 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                   $errorEmail = ' El formato de Email no es válido.'; 
-                   $errores = true;
+                    $errorEmail = ' El formato de Email no es válido.';
+                    $errores = true;
                 }
             }
-        }
-        
-        
-        /*--SANEADO--*/
-        function saneado($valor) {
-            $nuevoValor = trim($valor);
-            $nuevoValor = htmlspecialchars($nuevoValor);
-            return $nuevoValor;
-        }
-        
-        $telefono = saneado($_POST['telefono']);
-        $web = saneado($_POST['web']);
-                      
-        $db = new PDO('mysql:host=localhost;dbname=localidad;charset=utf8', 'root', '123456');
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        
-        if ($errores == false) {
-            $sql = "INSERT INTO contacto 
+            if (!empty($_POST['web'])) {
+                $telefono = saneado($_POST['web']);
+            }
+
+            
+
+            $db = new PDO('mysql:host=localhost;dbname=localidad;charset=utf8', 'root', '123456');
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+            if ($errores == false) {
+                $sql = "INSERT INTO contacto 
                     (nombre, telefono, email , web) 
                     VALUES  (?, ?, ?, ?)";
 
-            try {
-               $st = $db->prepare($sql);
-               $st->execute(array($nombre, $apellidos, $email, $web));
-            } catch (PDOException $e) {
-               echo $e->getMessage();
-               return false;
+                try {
+                    $st = $db->prepare($sql);
+                    $st->execute(array($nombre, $telefono, $email, $web));
+                    echo '<h4 style="text-align:center; color: black; background-color:#00ff66; height: 50px; margin-bottom: 40px"><br>Formulario enviado correctamente</h4>';
+                } catch (PDOException $e) {
+                    echo $e->getMessage();
+                    return false;
+                    
+                }
             }
-        } 
+        }
         ?>
-        
-        
+
+
         <div class="container" style="width: 58%; height: 500px">
             <h4 style="color: #999999; background-color: #ffffff; height: 50px; margin-bottom: 40px"><br>Introduzca la información que se solicita a continuación:</h4>
             <!-- Eliminamos action="guardar_contacto.php" para que se venga de vuelta -->
@@ -82,7 +99,7 @@
 
                 <div class="form-group">
 
-                    <label for = "nombre">Nombre* :</label><span><?php echo $errorNombre ?></span>
+                    <label for = "nombre">Nombre* :</label><span style="color: red;"><?php echo $errorNombre ?></span>
                     <input type = "text" name = "nombre" class = "form-control" id = "nombre" placeholder = "Nombre del solicitante"
                            value = "<?php echo isset($_POST['nombre']) ? $_POST['nombre'] : '' ?>">
                 </div>
@@ -94,13 +111,14 @@
                 </div>
 
                 <div class = "form-group">
-                    <br><label for = "email">Email corporativo* :</label><span><?php echo $errorEmail ?></span>
+                    <br><label for = "email">Email corporativo* :</label>
                     <input type = "text" name = "email" class = "form-control" id = "email" placeholder = "email de empresa" style = " width: 150px"
                            value = "<?php echo isset($_POST['email']) ? $_POST['email'] : '' ?>">
+                    <span style="color: red;"><?php echo $errorEmail ?></span>
                 </div><br>
 
                 <div class = "form-group">
-                    <label for = "web">Sitio web:</label>
+                    <label for = "web">Sitio web (Opcional):</label>
                     <input type = "text" name = "web" class = "form-control" id = "web" placeholder = "sitio web"
                            value = "<?php echo isset($_POST['web']) ? $_POST['web'] : '' ?>">
                 </div><br>
@@ -111,22 +129,22 @@
 
 
 
-            
-            <div class = botones style = " width: 185px;">
-                <input type = "button" onclick = "myFunction()" value = "Borrar" style = " cursor: pointer; width: 80px; height: 30px;
-                       background-color: #ff9999; box-shadow: 10px 5px 5px silver;  margin-bottom: 5px">
+
+                <div class = botones style = " width: 185px;">
+                    <input type = "button" onclick = "myFunction()" value = "Borrar" style = " cursor: pointer; width: 80px; height: 30px;
+                           background-color: #ff9999; box-shadow: 10px 5px 5px silver;  margin-bottom: 5px">
 
 
 
-                <input type = "submit" value = "Enviar" name = "submit" style = " cursor: pointer; width: 80px; height: 30px;
-                       background-color: #00ff66; box-shadow: 10px 5px 5px silver;  margin-left: 1px">
+                    <input type = "submit" value = "Enviar" name = "submit" style = " cursor: pointer; width: 80px; height: 30px;
+                           background-color: #00ff66; box-shadow: 10px 5px 5px silver;  margin-left: 1px">
 
-                <img src = "ajax-loader.gif" id = "loader" >
+                    <img src = "ajax-loader.gif" id = "loader" >
 
-                <!--<div class = "loader"></div>-->
+                    <!--<div class = "loader"></div>-->
 
-            </div>
-           </form>
+                </div>
+            </form>
         </div>
 
 
@@ -146,50 +164,50 @@
         <script src="patron.js"></script>
 
         <script>
-                    /*
-                     
-                     $(document).ready(function () {
-                     $('[type="submit"]').on('click', function (event) {
-                     
-                     event.preventDefault();
-                     
-                     //Añadimos la imagen de carga en el contenedor
-                     $('#loader').show();
-                     
-                     var campo_nombre = $('#nombre').attr('name');
-                     var valor_nombre = $('#nombre').val();
-                     
-                     var campo_telefono = $('#telefono').attr('name');
-                     var valor_telefono = $('#telefono').val();
-                     
-                     var campo_email = $('#email').attr('name');
-                     var valor_email = $('#email').val();
-                     
-                     var campo_web = $('#web').attr('name');
-                     var valor_web = $('#web').val();
-                     
-                     
-                     $.ajax({
-                     
-                     url: 'guardar_contacto.php',
-                     method: 'POST',
-                     data: {nombre: valor_nombre,
-                     telefono: valor_telefono,
-                     email: valor_email,
-                     web: valor_web
-                     },
-                     success: function (respuesta) {
-                     $('#loader').hide();
-                     alert("Formulario enviado");
-                     },
-                     error: function (respuesta) {
-                     alert("Formulario erroneo");
-                     }
-                     });
-                     
-                     });
-                     });
-                     */
+                        /*
+                         
+                         $(document).ready(function () {
+                         $('[type="submit"]').on('click', function (event) {
+                         
+                         event.preventDefault();
+                         
+                         //Añadimos la imagen de carga en el contenedor
+                         $('#loader').show();
+                         
+                         var campo_nombre = $('#nombre').attr('name');
+                         var valor_nombre = $('#nombre').val();
+                         
+                         var campo_telefono = $('#telefono').attr('name');
+                         var valor_telefono = $('#telefono').val();
+                         
+                         var campo_email = $('#email').attr('name');
+                         var valor_email = $('#email').val();
+                         
+                         var campo_web = $('#web').attr('name');
+                         var valor_web = $('#web').val();
+                         
+                         
+                         $.ajax({
+                         
+                         url: 'guardar_contacto.php',
+                         method: 'POST',
+                         data: {nombre: valor_nombre,
+                         telefono: valor_telefono,
+                         email: valor_email,
+                         web: valor_web
+                         },
+                         success: function (respuesta) {
+                         $('#loader').hide();
+                         alert("Formulario enviado");
+                         },
+                         error: function (respuesta) {
+                         alert("Formulario erroneo");
+                         }
+                         });
+                         
+                         });
+                         });
+                         */
         </script>
 
 
